9af2b5aae677ba8ddd93ef97860fe5f484df2c8e
Abd EL Rahman Sabri <a.sabri.soliman0@gmail.com>
2026-01-01 17:10:40 +0200

    define database and schema creation scripts

---
+++ 01-database-setup/create_database.sql
@@ -0,0 +1,10 @@
+-- =====================================================
+-- File: create_database.sql
+-- Purpose: Create main database for National Statistics DWH
+-- =====================================================
+
+CREATE DATABASE national_statistics_dwh
+WITH
+    OWNER = postgres,
+    ENCODING = 'UTF8';
+
---
+++ 01-database-setup/create_schemas.sql
@@ -0,0 +1,9 @@
+-- =====================================================
+-- File: create_schemas.sql
+-- Purpose: Create schemas for DWH layers
+-- =====================================================
+
+CREATE SCHEMA IF NOT EXISTS landing;
+CREATE SCHEMA IF NOT EXISTS staging;
+CREATE SCHEMA IF NOT EXISTS dwh;
+
